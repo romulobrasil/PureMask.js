0e81094084d601f0f195ec60286c83b9269e5605
Rômulo "Sir Rommer" Brasil <sir.rommer@gmail.com>
2014-05-16 22:17:43 -0300

    Update puremask.js

--- puremask.js
+++ puremask.js
@@ -1,38 +1,46 @@
-var cars = document.querySelector(".data");
-
-var maskForm = cars.dataset.mask;
-var tam = maskForm.length;
-cars.maxLength = tam;
-console.log("maskForm: " + maskForm);
-
-//var cars2 = document.getElementById("cars");
-var e = cars.value.length;
-console.log("e: " + e);
-    
-cars.addEventListener('keydown', function (e){
-	
-	if (e.keyCode === 8 || e.keyCode === 46) {
-        console.log("Apertou Backspace ou DEL");
-    } else {
-        formatar(maskForm);
-    }
-	
-});
+/* ========================================================================
+ * Formzin: formzin.js v0.13
+ * http://romulobrasil.com
+ * Copyright (c) 2014 Rômulo Brasil
+ * ========================================================================
+ */
  
- function formatar(mask){
-    var cars2 = document.querySelector(".data");;
+'use strict';
+var PureMask = function() {
+    var el = document.querySelector(".data");
 
-    var i = cars2.value.length;
+    var maskForm = el.dataset.mask;
+    el.maxLength = maskForm.length;
 
-    var saida = mask.substring(0,1);
-    var texto = mask.substring(i)
+    var e = el.value.length;
+        
+        return {
+               init : function() {
 
-    console.log("i: " + i);
-    console.log("mask: " + mask);
-    console.log("saida: " + saida);
-    console.log("texto: " + texto);
+                   el.addEventListener('keydown', function (e){
 
-    if (texto.substring(0,1) != saida) {
-        cars2.value += texto.substring(0,1);
-    }
-}
+                    if (e.keyCode === 8 || e.keyCode === 46) {
+                            console.log("Apertou Backspace ou DEL");
+                        } else {
+                            formatar(maskForm);
+                        }
+
+                    });
+
+                    function formatar(mask){
+                        var element = document.querySelector(".data");;
+
+                        var i = element.value.length;
+
+                        var saida = mask.substring(0,1);
+                        var texto = mask.substring(i)
+
+
+                        if (texto.substring(0,1) != saida) {
+                            element.value += texto.substring(0,1);
+                        }
+                    }
+               }
+        };
+}();
+PureMask.init();
