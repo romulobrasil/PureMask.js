740bbc62b36b8ba9cb0cbcce43cd33dbe7c1a3a8
Romulo <me@romulobrasil.com>
2016-09-25 11:32:05 -0300

    v0.3

--- src/js/puremask.js
+++ src/js/puremask.js
@@ -1,7 +1,7 @@
 /* ========================================================================
- * Pure Mask JS: puremask.js v0.1
- * http://romulobrasil.com
- * Copyright (c) 2014 Rômulo Brasil
+ * Pure Mask JS: puremask.js v0.3
+ * https://romulobrasil.com
+ * Copyright (c) 2016 Rômulo Brasil
  * ========================================================================
  */
  
@@ -10,40 +10,78 @@ var PureMask = function() {
     return {
         format : function(element, placeholder) {            
             var el = document.querySelector(element);
-            var maskForm = el.dataset.mask;
-            el.maxLength = maskForm.length;
-            var e = el.value.length;
+            var maskForm = '';
             
-            if(placeholder === true) {
+            if (el === undefined || el === null) {
+                console.log('PureMask.js: class não foi declarada corretamente: ' + element + '.');
+                return null;
+            }
+            
+            if (el.dataset.mask === undefined || el.dataset.mask === null) {
+                console.log('PureMask.js: data-mask não foi declarada na Tag com a class: ' + el.className + '.');
+            } else {
+                maskForm = el.dataset.mask;
+                el.maxLength = maskForm.length;
+            }
+            
+            if (placeholder === true) {
                 el.placeholder = maskForm;
             }
-
+            
+            var e = el.value.length;
+            
             el.addEventListener('keydown', function (e){
-                if (e.keyCode === 8 || e.keyCode === 46) {                    
-                    } else {
-                        formato(maskForm);
-                    }
-                });
-
-                function formato(mask) {
-                    var text = '';
-                    var data = el.value;
-                    var c, m, i, x;
-
-                    for (i = 0, x = 1; x && i < mask.length; ++i) {
-                        c = data.charAt(i);
-                        m = mask.charAt(i);
-
-                        switch (mask.charAt(i)) {
-                            case '#' : if (/\d/.test(c)) {text += c;} else {x = 0;} break;
-                            case 'A' : if (/[a-z]/i.test(c)) {text += c;} else {x = 0;} break;
-                            case 'N' : if (/[a-z0-9]/i.test(c)) {text += c;} else {x = 0;} break;
-                            case 'X' : text += c; break;
-                            default  : text += m; break;
-                        }
-                    }
-                    el.value = text;                
+                if (e.keyCode !== 8 || e.keyCode !== 46) {   
+                    formato(maskForm);
                 }
+            });
+            
+            function formato(mask) {
+                var text = '';
+                var data = el.value;
+                var c, m, i, x;
+
+                for (i = 0, x = 1; x && i < mask.length; ++i) {
+                    c = data.charAt(i);
+                    m = mask.charAt(i);
+
+                    switch (mask.charAt(i)) {
+                        case '#' : 
+                            if (/\d/.test(c)) {
+                                text += c;
+                            } else {
+                                x = 0;
+                            } 
+                            break;
+
+                        case 'A' : 
+                            if (/[a-z]/i.test(c)) {
+                                text += c;
+                            } else {
+                                x = 0;
+
+                            } 
+                            break;
+
+                        case 'N' : 
+                            if (/[a-z0-9]/i.test(c)) {
+                                text += c;
+                            } else {
+                                x = 0;
+                            } 
+                            break;
+
+                        case 'X' : 
+                            text += c; 
+                            break;
+
+                        default  : 
+                            text += m; 
+                            break;
+                    }
+                }
+                el.value = text;                
+            }
         }
     };
 }();
