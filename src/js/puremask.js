527b2f6df06e8eff93e1134250812763c4e96c60
Victor Aurélio <ovictoraurelio@gmail.com>
2017-04-25 18:22:19 -0300

    Adicionando suporte para formatar multiplos inputs com com um único seletor
    Agora é possível formatar vários campos com uma única seleção, muito útil quando tenho campos repetidos que precisam de uma mesma máscara e possuem uma class em comum.

--- src/js/puremask.js
+++ src/js/puremask.js
@@ -4,84 +4,86 @@
  * Copyright (c) 2016 Rômulo Brasil
  * ========================================================================
  */
- 
+
 'use strict';
 var PureMask = function() {
     return {
-        format : function(element, placeholder) {            
-            var el = document.querySelector(element);
-            var maskForm = '';
-            
-            if (el === undefined || el === null) {
-                console.log('PureMask.js: class não foi declarada corretamente: ' + element + '.');
-                return null;
-            }
-            
-            if (el.dataset.mask === undefined || el.dataset.mask === null) {
-                console.log('PureMask.js: data-mask não foi declarada na Tag com a class: ' + el.className + '.');
-            } else {
-                maskForm = el.dataset.mask;
-                el.maxLength = maskForm.length;
-            }
-            
-            if (placeholder === true) {
-                el.placeholder = maskForm;
-            }
-            
-            var e = el.value.length;
-            
-            el.addEventListener('keypress', function (e){
-                if (e.keyCode !== 8 || e.keyCode !== 46) {   
-                    formato(maskForm);
+        format : function(element, placeholder) {
+            var elements = document.querySelectorAll(element);
+            elements.forEach(function(el){
+                var maskForm = '';
+
+                if (el === undefined || el === null) {
+                    console.log('PureMask.js: class não foi declarada corretamente: ' + element + '.');
+                    return null;
+                }
+
+                if (el.dataset.mask === undefined || el.dataset.mask === null) {
+                    console.log('PureMask.js: data-mask não foi declarada na Tag com a class: ' + el.className + '.');
+                } else {
+                    maskForm = el.dataset.mask;
+                    el.maxLength = maskForm.length;
+                }
+
+                if (placeholder === true) {
+                    el.placeholder = maskForm;
+                }
+
+                var e = el.value.length;
+
+                el.addEventListener('keypress', function (e){
+                    if (e.keyCode !== 8 || e.keyCode !== 46) {
+                        formato(maskForm);
+                    }
+                });
+
+                function formato(mask) {
+                    var text = '';
+                    var data = el.value;
+                    var c, m, i, x;
+
+                    for (i = 0, x = 1; x && i < mask.length; ++i) {
+                        c = data.charAt(i);
+                        m = mask.charAt(i);
+
+                        switch (mask.charAt(i)) {
+                            case '#' :
+                                if (/\d/.test(c)) {
+                                    text += c;
+                                } else {
+                                    x = 0;
+                                }
+                                break;
+
+                            case 'A' :
+                                if (/[a-z]/i.test(c)) {
+                                    text += c;
+                                } else {
+                                    x = 0;
+
+                                }
+                                break;
+
+                            case 'N' :
+                                if (/[a-z0-9]/i.test(c)) {
+                                    text += c;
+                                } else {
+                                    x = 0;
+                                }
+                                break;
+
+                            case 'X' :
+                                text += c;
+                                break;
+
+                            default  :
+                                text += m;
+                                break;
+                        }
+                    }
+                    el.value = text;
                 }
             });
-            
-            function formato(mask) {
-                var text = '';
-                var data = el.value;
-                var c, m, i, x;
-
-                for (i = 0, x = 1; x && i < mask.length; ++i) {
-                    c = data.charAt(i);
-                    m = mask.charAt(i);
-
-                    switch (mask.charAt(i)) {
-                        case '#' : 
-                            if (/\d/.test(c)) {
-                                text += c;
-                            } else {
-                                x = 0;
-                            } 
-                            break;
-
-                        case 'A' : 
-                            if (/[a-z]/i.test(c)) {
-                                text += c;
-                            } else {
-                                x = 0;
-
-                            } 
-                            break;
-
-                        case 'N' : 
-                            if (/[a-z0-9]/i.test(c)) {
-                                text += c;
-                            } else {
-                                x = 0;
-                            } 
-                            break;
-
-                        case 'X' : 
-                            text += c; 
-                            break;
-
-                        default  : 
-                            text += m; 
-                            break;
-                    }
-                }
-                el.value = text;                
-            }
         }
     };
 }();
